3903c5cbe6171fe4baadb064f99711b8069d2c12
Jakub Trllo <43494761+iLLiCiTiT@users.noreply.github.com>
2024-05-22 17:17:30 +0200

    move tvpaint client code next to server codebase

---
+++ client/ayon_tvpaint/__init__.py
@@ -0,0 +1,12 @@
+from .addon import (
+    get_launch_script_path,
+    TVPaintAddon,
+    TVPAINT_ROOT_DIR,
+)
+
+
+__all__ = (
+    "get_launch_script_path",
+    "TVPaintAddon",
+    "TVPAINT_ROOT_DIR",
+)
---
+++ client/ayon_tvpaint/api/__init__.py
@@ -0,0 +1,11 @@
+from .communication_server import CommunicationWrapper
+from .pipeline import (
+    TVPaintHost,
+)
+
+
+__all__ = (
+    "CommunicationWrapper",
+
+    "TVPaintHost",
+)
