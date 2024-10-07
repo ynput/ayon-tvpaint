438fae0a639018fe2925119de1e713885f3ebe95
Roy Nieterau <roy_nieterau@hotmail.com>
2024-10-07 22:58:36 +0200

    Define app host name

--- package.py
+++ package.py
@@ -1,8 +1,10 @@
 name = "tvpaint"
 title = "TVPaint"
 version = "0.2.4-dev.1"
+app_host_name = "tvpaint"
 client_dir = "ayon_tvpaint"
 
+ayon_server_version = ">=1.1.2"
 ayon_required_addons = {
     "core": ">0.3.2",
 }
