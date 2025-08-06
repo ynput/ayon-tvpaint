f69fa70cbcac90682fde2e1312ed526fed55a0b0
Jakub Trllo <43494761+iLLiCiTiT@users.noreply.github.com>
2025-08-06 09:53:38 +0200

    formatting changes

--- client/ayon_tvpaint/lib.py
+++ client/ayon_tvpaint/lib.py
@@ -543,9 +543,12 @@ def cleanup_rendered_layers(filepaths_by_layer_id):
 
 
 def composite_rendered_layers(
-    layers_data, filepaths_by_layer_id,
-    range_start, range_end,
-    dst_filepaths_by_frame, cleanup=True
+    layers_data,
+    filepaths_by_layer_id,
+    range_start,
+    range_end,
+    dst_filepaths_by_frame,
+    cleanup=True,
 ):
     """Composite multiple rendered layers by their position.
 
@@ -566,6 +569,7 @@ def composite_rendered_layers(
             image after compositing will be stored. Path must not clash with
             source filepaths.
         cleanup(bool): Remove all source filepaths when done with compositing.
+
     """
     # Prepare layers by their position
     #   - position tells in which order will compositing happen
