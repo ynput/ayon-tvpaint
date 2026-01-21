e33e8cf1613476a03d1a41a1138ea80a939fcd6f
Jakub Trllo <43494761+iLLiCiTiT@users.noreply.github.com>
2026-01-21 12:30:10 +0100

    use explicit kwargs for 'CreatedInstance'

--- client/ayon_tvpaint/plugins/create/create_render.py
+++ client/ayon_tvpaint/plugins/create/create_render.py
@@ -239,10 +239,10 @@ class CreateRenderlayer(TVPaintCreator):
 
         self.log.info(f"Product name is {product_name}")
         new_instance = CreatedInstance(
-            self.product_type,
-            product_name,
-            instance_data,
-            self
+            product_type=self.product_base_type,
+            product_name=product_name,
+            data=instance_data,
+            creator=self
         )
         self._store_new_instance(new_instance)
 
@@ -706,10 +706,10 @@ class CreateRenderPass(TVPaintCreator):
         )
 
         new_instance = CreatedInstance(
-            self.product_type,
-            product_name,
-            instance_data,
-            self
+            product_type=self.product_base_type,
+            product_name=product_name,
+            data=instance_data,
+            creator=self,
         )
         instances_data = self._remove_and_filter_instances(
             instances_to_remove
@@ -1455,7 +1455,10 @@ class TVPaintSceneRenderCreator(TVPaintAutoCreator):
             data["active"] = False
 
         new_instance = CreatedInstance(
-            self.product_type, product_name, data, self
+            product_type=self.product_base_type,
+            product_name=product_name,
+            data=data,
+            creator=self,
         )
         instances_data = self.host.list_instances()
         instances_data.append(new_instance.data_to_store())
