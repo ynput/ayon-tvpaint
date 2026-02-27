768218bd94047c9b2554ea35487f1cc9f257f235
Jakub Trllo <43494761+iLLiCiTiT@users.noreply.github.com>
2026-02-27 17:37:59 +0100

    use product base type during publishign

--- client/ayon_tvpaint/plugins/publish/extract_sequence.py
+++ client/ayon_tvpaint/plugins/publish/extract_sequence.py
@@ -120,7 +120,7 @@ class ExtractSequence(pyblish.api.InstancePlugin):
             "Files will be rendered to folder: {}".format(output_dir)
         )
 
-        if instance.data["productType"] == "review":
+        if instance.data["productBaseType"] == "review":
             result = self.render_review(
                 output_dir, mark_in, mark_out, scene_bg_color
             )
